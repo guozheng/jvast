dc6faf6cf485aef22d50834b22425dba2889a502
Guozheng Ge <guozheng.ge@gmail.com>
2022-06-14 16:40:06 +0800

    fix gradle build issues

--- lib/src/main/java/jvast/util/StringUtil.java
+++ lib/src/main/java/jvast/util/StringUtil.java
@@ -9,12 +9,11 @@ import java.util.regex.Pattern;
 import lombok.experimental.UtilityClass;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.util.Strings;
 
 @UtilityClass
 public class StringUtil {
   private static final Logger LOGGER = LogManager.getLogger(StringUtil.class);
-  public final String EMPTY = "";
+  public static final String EMPTY = "";
 
   /**
    * Match all strings in the VAST doc using a pattern.
